a65cc5713167650b9060ef3968a5ae7752411ab6
Franklin Esteves <festeves@outlook.com>
2023-11-28 00:24:52 -0500

    Listado personas con Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listado-personas';
+  titulo = 'Listado de Peronas'
+  nombreInput:string ='';
+  apellidoInput:string =''
+
+
+  personas:Persona[] = [ new Persona('Franklin','Esteves') , new Persona('Jennifer', 'Sanchez') ]
+
+
+  agregarPersona():void{
+    let persona = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona);
+  }
+
 }
